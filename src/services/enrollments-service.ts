939f8be211e4ce7907118c81f0758e73e2b6d673
gpsign <gpsignorini@gmail.com>
2023-09-18 01:34:39 -0300

    feat: cep

--- src/services/enrollments-service.ts
+++ src/services/enrollments-service.ts
@@ -1,69 +1,97 @@
-import { Address, Enrollment } from '@prisma/client';
-import { request } from '@/utils/request';
-import { notFoundError } from '@/errors';
-import { addressRepository, CreateAddressParams, enrollmentRepository, CreateEnrollmentParams } from '@/repositories';
-import { exclude } from '@/utils/prisma-utils';
+import { Address, Enrollment } from "@prisma/client";
+import { request } from "@/utils/request";
+import { invalidDataError, notFoundError, requestError } from "@/errors";
+import {
+	addressRepository,
+	CreateAddressParams,
+	enrollmentRepository,
+	CreateEnrollmentParams,
+} from "@/repositories";
+import { exclude } from "@/utils/prisma-utils";
 
-// TODO - Receber o CEP por parâmetro nesta função.
-async function getAddressFromCEP() {
-  // FIXME: está com CEP fixo!
-  const result = await request.get(`${process.env.VIA_CEP_API}/37440000/json/`);
+async function getAddressFromCEP(cep: string) {
+	if (cep.length != 8 || isNaN(Number(cep))) throw invalidDataError("Zip Code");
 
-  // TODO: Tratar regras de negócio e lanças eventuais erros
+	const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
-  // FIXME: não estamos interessados em todos os campos
-  return result.data;
+	console.log(result.data.erro);
+
+	if (result.data.erro) throw requestError(400, "Non-existent Zip Code");
+
+	// FIXME: não estamos interessados em todos os campos
+	return result.data.cidade;
 }
 
-async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
-  const enrollmentWithAddress = await enrollmentRepository.findWithAddressByUserId(userId);
+async function getOneWithAddressByUserId(
+	userId: number
+): Promise<GetOneWithAddressByUserIdResult> {
+	const enrollmentWithAddress =
+		await enrollmentRepository.findWithAddressByUserId(userId);
 
-  if (!enrollmentWithAddress) throw notFoundError();
+	if (!enrollmentWithAddress) throw notFoundError();
 
-  const [firstAddress] = enrollmentWithAddress.Address;
-  const address = getFirstAddress(firstAddress);
+	const [firstAddress] = enrollmentWithAddress.Address;
+	const address = getFirstAddress(firstAddress);
 
-  return {
-    ...exclude(enrollmentWithAddress, 'userId', 'createdAt', 'updatedAt', 'Address'),
-    ...(!!address && { address }),
-  };
+	return {
+		...exclude(
+			enrollmentWithAddress,
+			"userId",
+			"createdAt",
+			"updatedAt",
+			"Address"
+		),
+		...(!!address && { address }),
+	};
 }
 
-type GetOneWithAddressByUserIdResult = Omit<Enrollment, 'userId' | 'createdAt' | 'updatedAt'>;
+type GetOneWithAddressByUserIdResult = Omit<
+	Enrollment,
+	"userId" | "createdAt" | "updatedAt"
+>;
 
 function getFirstAddress(firstAddress: Address): GetAddressResult {
-  if (!firstAddress) return null;
+	if (!firstAddress) return null;
 
-  return exclude(firstAddress, 'createdAt', 'updatedAt', 'enrollmentId');
+	return exclude(firstAddress, "createdAt", "updatedAt", "enrollmentId");
 }
 
-type GetAddressResult = Omit<Address, 'createdAt' | 'updatedAt' | 'enrollmentId'>;
+type GetAddressResult = Omit<
+	Address,
+	"createdAt" | "updatedAt" | "enrollmentId"
+>;
 
-async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollmentWithAddress) {
-  const enrollment = exclude(params, 'address');
-  enrollment.birthday = new Date(enrollment.birthday);
-  const address = getAddressForUpsert(params.address);
+async function createOrUpdateEnrollmentWithAddress(
+	params: CreateOrUpdateEnrollmentWithAddress
+) {
+	const enrollment = exclude(params, "address");
+	enrollment.birthday = new Date(enrollment.birthday);
+	const address = getAddressForUpsert(params.address);
 
-  // TODO - Verificar se o CEP é válido antes de associar ao enrollment.
+	// TODO - Verificar se o CEP é válido antes de associar ao enrollment.
 
-  const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, 'userId'));
+	const newEnrollment = await enrollmentRepository.upsert(
+		params.userId,
+		enrollment,
+		exclude(enrollment, "userId")
+	);
 
-  await addressRepository.upsert(newEnrollment.id, address, address);
+	await addressRepository.upsert(newEnrollment.id, address, address);
 }
 
 function getAddressForUpsert(address: CreateAddressParams) {
-  return {
-    ...address,
-    ...(address?.addressDetail && { addressDetail: address.addressDetail }),
-  };
+	return {
+		...address,
+		...(address?.addressDetail && { addressDetail: address.addressDetail }),
+	};
 }
 
 export type CreateOrUpdateEnrollmentWithAddress = CreateEnrollmentParams & {
-  address: CreateAddressParams;
+	address: CreateAddressParams;
 };
 
 export const enrollmentsService = {
-  getOneWithAddressByUserId,
-  createOrUpdateEnrollmentWithAddress,
-  getAddressFromCEP,
+	getOneWithAddressByUserId,
+	createOrUpdateEnrollmentWithAddress,
+	getAddressFromCEP,
 };
